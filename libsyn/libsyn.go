7100156219aac350bfac2ec58110f03653ed987a
Hilde N <snhilde@protonmail.com>
2020-05-25 14:17:06 -0400

    add Handles, remove Init

--- libsyn/libsyn.go
+++ libsyn/libsyn.go
@@ -1,37 +1,27 @@
 package libsyn
 
 import (
+	"net/url"
+	"strings"
 	"net/http"
 	"io/ioutil"
 )
 
 
-// Show is the main type. It holds information necessary to download the show's RSS feed.
-type Show struct {
-	url string
-}
-
-
-func Init(url string) *Show {
-	s := new(Show)
-	s.url = url
-
-	return s
-}
-
-func (s *Show) Feed() []byte {
-	// Grab the show's RSS feed.
-	resp, err := http.Get(s.url + "/rss")
-	if err != nil {
-		return nil
-	}
-	defer resp.Body.Close()
-
-	// Pull out the content.
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil
+// Handles determines if the provided url should be handled by this module or not.
+func Handles(u *url.URL) bool {
+	// The hostname will look something like this:
+	// <show name>.libsyn.com
+	host := u.Hostname()
+	parts := strings.Split(host, ".")
+	if parts[len(parts) - 2] == "libsyn" {
+		return true
 	}
 
-	return body
+	return false
+}
+
+// Feed grabs the raw XML of the show's RSS feed.
+func Feed(u *url.URL) ([]byte, error) {
+
 }
