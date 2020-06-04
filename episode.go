c97fa9914e02be9280844da164633fdff871a6f6
Hilde N <snhilde@protonmail.com>
2020-06-04 19:35:11 -0400

    add more members to Episode, add Write, remove UnmarshmalXML, add addFrames

--- episode.go
+++ episode.go
@@ -6,23 +6,35 @@ import (
 	"path/filepath"
 	"os"
 	"net/http"
-	"encoding/xml"
-	"github.com/kennygrant/sanitize"
-	"strconv"
+	"time"
 )
 
 
 // Episode represents internal data related to each episode of the podcast.
-type Episode struct {
-	Number   int       // episode number
-	Title    string    // title of the episode
-	Link     string    // link used to download the episode
-	Length   int       // episode size in bytes
-	Ext      string    // file extension
+type Episode  struct {
+	// Show information
+	showTitle   string
+	showArtist  string
+
+	// Episode information
+	Title       string    `xml:">title"`
+	Season      string    `xml:">season"`
+	Number      string    `xml:">episode"`
+	Desc        string    `xml:">description"`
+	Date        string    `xml:">pubDate"`
+	URL         string    `xml:">enclosure,url"`
+	Size        string    `xml:">enclosure,length"` // TODO: currently unused
+	Type        string    `xml:">enclosure,type"`
+
+	// Objects to handle reading/writing
+	meta       *Meta      // Metadata object
+	w           io.Writer // Writer that will handle writing the file.
 }
 
 
-// Download downloads the episode.
+// Download downloads the episode. The bytes will stream through this path from web to disk:
+// Internet -> http object -> Meta object -> Episode object -> Disk
+//                 \-> Progress Bar object
 func (e *Episode) Download(showDir string) error {
 	if showDir == "" {
 		return fmt.Errorf("Invalid call")
@@ -31,34 +43,30 @@ func (e *Episode) Download(showDir string) error {
 	filename := filepath.Join(showDir, e.Title)
 	fmt.Println(filename)
 
-	// Create a save point.
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	// Grab the file's data.
-	resp, err := http.Get(e.Link)
+	resp, err := http.Get(e.URL)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	// Make sure we accessed everything correctly.
 	if resp.StatusCode != 200 {
 		return fmt.Errorf("%v", resp.Status)
 	}
 
-	// Set up our progress bar.
 	bar := ProgressBar{total: int(resp.ContentLength), totalString: Reduce(int(resp.ContentLength))}
 	tee := io.TeeReader(resp.Body, &bar)
 
-	// Wrap the file writer in a metadata writer so we can add/modify the tag data.
-	wrapper := NewMeta(e, file)
+	// Connect the episode on both ends of the flow.
+	e.meta = NewMeta(nil)
+	e.w = file
 
-	// Save the file.
-	_, err = io.Copy(wrapper, tee)
+	_, err = io.Copy(e, tee)
 	if err != nil {
 		return err
 	}
@@ -66,105 +74,105 @@ func (e *Episode) Download(showDir string) error {
 	// Because we've been mucking around with carriage returns, we need to manually move down a row.
 	fmt.Println()
 
-	// Make sure there wasn't a problem writing the metadata.
-	if !wrapper.Written() {
-		return fmt.Errorf("Unexpectedly failed to write metadata")
-	}
-
 	return nil
 }
 
-// For the <title> tag, there are two tags returned: a general one, and one in the "itunes" namespace. The general tag
-// usually has an episode number, while the other one does not. It will make everything easier if we save the podcast
-// with the episode number in the filename, so we want to prefer the general tag. However, the parser reads the "itunes"
-// namespace tag after the general one and overwrites the saved data, thus clobbering the desired title. This issue has
-// been discussed as needing a fix in the xml library for some time. We'll grab the correct one here.
-func (e *Episode) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+// Write first constructs and then writes the episode's metadata and then passes all remaining data on to the next layer.
+func (e *Episode) Write(p []byte) (int, error) {
 	if e == nil {
-		return fmt.Errorf("Invalid receiver")
+		return 0, fmt.Errorf("Invalid episode object")
+	} else if e.w == nil {
+		return 0, fmt.Errorf("Invalid writer")
 	}
 
-	// Scan through all of the tokens.
-	for t, err := d.Token(); err != io.EOF; t, err = d.Token() {
-		if err != nil {
-			return err
+	consumed := 0
+	if !e.meta.Buffered() {
+		// Continue buffering metadata.
+		if n, err := e.meta.Write(p); err != io.ErrShortWrite {
+			// Either more data is needed or there was an error writing the metadata.
+			return n, err
+		} else {
+			// All metadata has been written. The rest of the bytes are filedata.
+			consumed = n
 		}
 
-		// When we find a start tag, we'll see if we have one of the tags that we want to save.
-		if s, ok := t.(xml.StartElement); ok {
-			// xml.StartElement has two fields: Name and Attr. We only care about the name, of which there are two
-			// parts: Tag name (Local) and namespace (Space).
-			switch s.Name.Local {
-			case "title":
-				// We only want the <title> tag without a namespace.
-				if s.Name.Space == "" {
-					// Grab the text between the opening and closing tags.
-					n, err := d.Token()
-					if err != nil {
-						return err
-					}
-					if cd, ok := n.(xml.CharData); ok {
-						e.Title = sanitize.BaseName(string(cd))
-					}
+		// Now that we have all of the metadata, let's build it with the additional data from the episode and write
+		// everything to disk.
+		e.addFrames()
+		metadata := e.meta.Build()
+		if n, err := e.w.Write(metadata); err != nil {
+			return consumed, err
+		} else if n != len(metadata) {
+			return consumed, fmt.Errorf("Failed to write complete metadata")
+		}
+	}
+
+	// If we're here, then all metadata has been successfully written. We can resume with writing the file data now.
+	return e.w.Write(p[consumed:])
+}
+
+// SetShowTitle sets the title of the episode's show.
+func (e *Episode) SetShowTitle(title string) {
+	if e != nil {
+		e.showTitle = title
+	}
+}
+
+// SetShowArtist sets the artist of the episode's show.
+func (e *Episode) SetShowArtist(artist string) {
+	if e != nil {
+		e.showArtist = artist
+	}
+}
+
+
+// addFrames fleshes out the metadata with information from the episode. If a field already exists in the metadata, it
+// will not be overwritten with data from the RSS feed.
+func (e *Episode) addFrames() {
+	frames := []struct {
+		field string
+		value string
+	}{
+		// Show information
+		{ "TALB", e.showTitle  },
+		{ "TPE1", e.showArtist }, // Artist
+		{ "TPE2", e.showArtist }, // Album Artist
+
+		// Episode information
+		{ "TIT2", e.Title      },
+		{ "TRCK", e.Number     },
+		{ "TDES", e.Desc       },
+		{ "TPOS", e.Season     },
+		{ "WOAF", e.URL        },
+
+		// Defaults
+		{ "TCON", "Podcast"    },
+		{ "PCST", "1"          },
+	}
+
+	for _, frame := range frames {
+		if e.meta.GetField(frame.field) == "" {
+			e.meta.SetField(frame.field, frame.value)
+		}
+	}
+
+	if e.Date != "" {
+		if time, err := time.Parse("Mon, 02 Jan 2006 15:04:05 -0700", e.Date); err == nil {
+			if e.meta.Version() == "3" {
+				if e.meta.GetField("TYER") == "" {
+					e.meta.SetField("TYER", time.Format("2006")) // YYYY
 				}
-			case "enclosure":
-				// We don't care about namespaces for this tag.
-				for _, v := range s.Attr {
-					switch v.Name.Local {
-					case "url":
-						e.Link = v.Value
-					case "length":
-						bytes, err := strconv.Atoi(v.Value)
-						if err != nil {
-							return err
-						}
-						e.Length = bytes
-					case "type":
-						e.Ext = mimeToExt(v.Value)
-					}
+				if e.meta.GetField("TDAT") == "" {
+					e.meta.SetField("TDAT", time.Format("0201")) // DDMM
 				}
-			case "episode":
-				// For this tag, we actually want the namespace. Currently, I have only seen an itunes namespace, but
-				// we'll allow any others that pop up.
-				if s.Name.Space != "" {
-					n, err := d.Token()
-					if err != nil {
-						return err
-					}
-					if cd, ok := n.(xml.CharData); ok {
-						if num, err := strconv.Atoi(string(cd)); err != nil {
-							return err
-						} else {
-							e.Number = num
-						}
-					}
+				if e.meta.GetField("TIME") == "" {
+					e.meta.SetField("TIME", time.Format("1504")) // HHMM
+				}
+			} else {
+				if e.meta.GetField("TDRC") == "" {
+					e.meta.SetField("TDRC", time.Format("20060102T150405")) // YYYYMMDDTHHMMSS
 				}
 			}
 		}
 	}
-
-	return nil
-}
-
-// mimeToExt finds the appropriate file extension based on the MIME type.
-func mimeToExt(mime string) string {
-	switch mime {
-	case "audio/aac":
-		return ".aac"
-	case "audio/midi", "audio/x-midi":
-		return ".midi"
-	case "audio/mpeg", "audio/mp3":
-		return ".mp3"
-	case "audio/ogg":
-		return ".oga"
-	case "audio/opus":
-		return ".opus"
-	case "audio/wav":
-		return ".wav"
-	case "audio/webm":
-		return ".weba"
-	}
-
-	// If we can't match a specific type, we'll default to mp3.
-	return ".mp3"
 }
